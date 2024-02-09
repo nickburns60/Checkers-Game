54e2dd43688eff82d102e410aeea5633f2c71b67
Nick Burns <nickrburns60@gmail.com>
2024-02-09 16:47:51 -0500

    Updated format of board

--- CheckersGame/src/main/java/org/checkers/CheckerBoard.java
+++ CheckersGame/src/main/java/org/checkers/CheckerBoard.java
@@ -4,16 +4,16 @@ public class CheckerBoard {
     public String toString(){
         //create starting board
         String checkerBoard = ("""
-               |-------------------|
-               |    | o o o o|     |
-               |    |o o o o |     |
-               |    | o o o o|     |
-               |    |        |     |
-               |    |        |     |
-               |    |o o o o |     |
-               |    | o o o o|     |
-               |    |o o o o |     |
-               |-------------------|
+               +-------------------+
+               +    | o o o o|     +
+               +    |o o o o |     +
+               +    | o o o o|     +
+               +    |        |     +
+               +    |        |     +
+               +    |o o o o |     +
+               +    | o o o o|     +
+               +    |o o o o |     +
+               +-------------------+
                 
                 """);
         return checkerBoard;
