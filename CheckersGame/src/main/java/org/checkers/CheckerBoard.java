748c11cda87d258c74be5a4e5704a0b677c7e6a7
Nick Burns <nickrburns60@gmail.com>
2024-02-09 15:58:22 -0500

    Created a class for the board and made a basic table to display starting board setup.

---
+++ CheckersGame/src/main/java/org/checkers/CheckerBoard.java
@@ -0,0 +1,21 @@
+package org.checkers;
+
+public class CheckerBoard {
+    public String toString(){
+        //create starting board
+        String checkerBoard = ("""
+               |-------------------|
+               |    | o o o o|     |
+               |    |o o o o |     |
+               |    | o o o o|     |
+               |    |        |     |
+               |    |        |     |
+               |    |o o o o |     |
+               |    | o o o o|     |
+               |    |o o o o |     |
+               |-------------------|
+                
+                """);
+        return checkerBoard;
+    }
+}
--- CheckersGame/src/main/java/org/checkers/Main.java
+++ CheckersGame/src/main/java/org/checkers/Main.java
@@ -1,9 +1,12 @@
 package org.checkers;
 
+
 public class Main {
     public static void main(String[] args) {
-        while (true){
-            
-        }
+        CheckerBoard checkerBoard = new CheckerBoard();
+        System.out.println(checkerBoard);
+      //  while (true){
+
+       // }
     }
 }
