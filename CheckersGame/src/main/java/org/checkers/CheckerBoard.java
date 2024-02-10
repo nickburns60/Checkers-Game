f57f627b451eeb4f818c9a11f17b3de796755829
Nick Burns <nickrburns60@gmail.com>
2024-02-10 00:16:20 -0500

    Changed board format to show grid positions and made one side x's and the other o's

--- CheckersGame/src/main/java/org/checkers/CheckerBoard.java
+++ CheckersGame/src/main/java/org/checkers/CheckerBoard.java
@@ -3,19 +3,23 @@ package org.checkers;
 public class CheckerBoard {
     public String toString(){
         //create starting board
+        //Board displays grid points (A-H and 1-9) but I'm still unsure of a good way to assign each checker its own
+        //variable / value
         String checkerBoard = ("""
                +-------------------+
-               +    | o o o o|     +
-               +    |o o o o |     +
-               +    | o o o o|     +
-               +    |        |     +
-               +    |        |     +
-               +    |o o o o |     +
-               +    | o o o o|     +
-               +    |o o o o |     +
+               +-----12345678------+
+               +   A| o o o o|     +
+               +   B|o o o o |     +
+               +   C| o o o o|     +
+               +   D|        |     +
+               +   E|        |     +
+               +   F|x x x x |     +
+               +   G| x x x x|     +
+               +   H|x x x x |     +
                +-------------------+
                 
                 """);
         return checkerBoard;
+
     }
 }
