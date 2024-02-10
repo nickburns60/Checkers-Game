df753338721b02ead4efcdb47f0a174880f6cce3
Beau Blevins <beaucblevins@gmail.com>
2024-02-10 08:54:33 -0500

    Added note about potential way to create checker pieces and move them

--- CheckersGame/src/main/java/org/checkers/CheckerPieces.java
+++ CheckersGame/src/main/java/org/checkers/CheckerPieces.java
@@ -14,6 +14,13 @@ public class CheckerPieces {
      /*
      The child classes ideally would extend this class and use a setter to set values for each of their own 12 pieces.
      It would also be great if we could color code the pieces so they can all be o's instead of x and o's.
+
+     BEAU - Maybe we could create a method in each subclass that instantiates and names 12 objects from each subclass and
+     then puts them into a list? Then we wouldn't need to have local variables in the Main class and we could just use
+     class methods to move pieces.
+       - for example, each subclass could have a method called movePiece(String pieceName, String destination) that would
+       take the desired piece and move it to the desired destination. We would need some logic in there to make sure it
+       is a legal move though.
      */
     protected List<String> pieces = new ArrayList<>(12);
 
